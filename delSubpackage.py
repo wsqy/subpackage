b5076efeee562161f738bdde68f83011c726c738
qy <1323800521@qq.com>
2016-08-16 16:52:24 +0800

    完善删除日志

--- delSubpackage.py
+++ delSubpackage.py
@@ -7,6 +7,14 @@ import settings
 import task
 
 
+def del_remove(info):
+    os.remove(info)
+
+
+def del_system(info):
+    os.system("rm -f %s" % info)
+
+
 def remove_sub():
     log_path = "log/del.log"
     task_subpackage_set = settings.task_subpackage_set + ":" + getMyIP.get_intranet_ip()
@@ -17,11 +25,11 @@ def remove_sub():
         if rad_num:
             with open(log_path, "a+") as f:
                 f.write("get apk : %s\n" % rad_num)
-            print("get apk : %s" % rad_num)
             os.remove(rad_num)
             rem_set(task_subpackage_set, rad_num)
     except TypeError as e:
-        print("sleep %s s....." % settings.sleep_time)
+        with open(log_path, "a+") as f:
+            f.write("%s\t, sleep %s s....." % (e, settings.sleep_time))
         time.sleep(settings.sleep_time)
 
 if __name__ == "__main__":
