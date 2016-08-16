2344e5d34d996075b486593940db3315975902eb
qy <1323800521@qq.com>
2016-08-16 16:43:44 +0800

    修改删除方式

--- delSubpackage.py
+++ delSubpackage.py
@@ -8,25 +8,25 @@ import task
 
 
 def remove_sub():
+    log_path = "log/del.log"
     task_subpackage_set = settings.task_subpackage_set + ":" + getMyIP.get_intranet_ip()
     rad_num = task.get_task_hand_way("random_member")
     rem_set = task.get_task_hand_way("rem_set")
-    while True:
-        try:
-            rad_num = rad_num(task_subpackage_set)
-            if not rad_num:
-                print("no data,sleep %s s....." % settings.sleep_time)
-                time.sleep(settings.sleep_time)
-                continue
+    try:
+        rad_num = rad_num(task_subpackage_set)
+        if rad_num:
+            with open(log_path, "a+") as f:
+                f.write("get apk : %s\n" % rad_num)
             print("get apk : %s" % rad_num)
             os.remove(rad_num)
-            rem_set = rem_set(task_subpackage_set, rad_num)
-        except TypeError as e:
-            print("sleep %s s....." % settings.sleep_time)
-            time.sleep(settings.sleep_time)
-            continue
+            rem_set(task_subpackage_set, rad_num)
+    except TypeError as e:
+        print("sleep %s s....." % settings.sleep_time)
+        time.sleep(settings.sleep_time)
 
 if __name__ == "__main__":
-    remove_sub()
+    while True:
+        remove_sub()
+        time.sleep(settings.sleep_time)
 
 
